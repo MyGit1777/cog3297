9bdbecdf413fe688fa266190c33a628909b20737
MyGit1777 <myeducation5102@gmail.com>
2022-11-23 13:55:11 +0530

    Data loader spring boot initial commit.

---
+++ DataLoaderPortalPortal/src/main/java/com/dataloaderportal/DataLoaderPortalPortalApplication.java
@@ -0,0 +1,13 @@
+package com.dataloaderportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLoaderPortalPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataLoaderPortalPortalApplication.class, args);
+	}
+
+}
---
+++ DataLoaderPortalPortal/src/test/java/com/dataloaderportal/DataLoaderPortalPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataloaderportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLoaderPortalPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
