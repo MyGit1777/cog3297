11986d3daf718b9b094bd9ee4cf16a078d683538
MyGit1777 <myeducation5102@gmail.com>
2022-11-28 17:13:14 +0530

    Adding patient service with all endpoints.

---
+++ PatientService/src/main/java/com/patientservice/controller/PatientController.java
@@ -0,0 +1,58 @@
+package com.patientservice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.patientservice.helper.ExcelToDBHelper;
+import com.patientservice.model.Patient;
+import com.patientservice.service.PatientService;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/patient")
+public class PatientController {
+
+	@Autowired
+	private PatientService patientService;
+
+	@PostMapping("/uploadData")
+	public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file) {
+		if (ExcelToDBHelper.checkFileFormat(file)) {
+
+			this.patientService.savePatientData(file);
+
+			return ResponseEntity.ok("Data saved successfully");
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Something went wrong, please check file format");
+	}
+
+	@GetMapping("/getAllPatients")
+	public List<Patient> getAllPatients() {
+		return this.patientService.getAllPatients();
+	}
+
+	@GetMapping("/getByPatientName/{patientName}")
+	public ResponseEntity<Object> getPatientByName(@PathVariable("patientName") String patientName) {
+		return new ResponseEntity<>(this.patientService.getPatientByName(patientName), HttpStatus.OK);
+	}
+	
+	@PutMapping("/updatePatient")
+	public ResponseEntity<Object> updatePatient(@RequestBody Patient patient) {
+
+		this.patientService.updatePatient(patient);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+}
---
+++ PatientService/src/main/java/com/patientservice/helper/ExcelToDBHelper.java
@@ -0,0 +1,113 @@
+package com.patientservice.helper;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.patientservice.model.Patient;
+
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+public class ExcelToDBHelper {
+
+	// Check file type
+	public static boolean checkFileFormat(MultipartFile file) {
+
+		String contentType = file.getContentType();
+
+		if (contentType.equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	// Extracting list of Patients from excel sheet
+
+	public static List<Patient> extractListOfPatients(InputStream input) {
+		List<Patient> patientsList = new ArrayList<>();
+
+		try {
+
+			XSSFWorkbook workbook = new XSSFWorkbook(input);
+
+			XSSFSheet sheet = workbook.getSheet("P1");
+
+			int rowNumber = 0;
+			Iterator<Row> iterator = sheet.iterator();
+
+			while (iterator.hasNext()) {
+				Row row = iterator.next();
+
+				if (rowNumber == 0) {
+					rowNumber++;
+					continue;
+				}
+
+				Iterator<Cell> cells = row.iterator();
+
+				int cellId = 0;
+
+				Patient patient = new Patient();
+				SimpleDateFormat dateFormatter = new SimpleDateFormat("MM/dd/yyyy");
+				while (cells.hasNext()) {
+					Cell cell = cells.next();
+
+					switch (cellId) {
+//					case 0:
+//						patient.setPatientId(Long.valueOf(cell.getStringCellValue()));
+//						break;
+					case 0:
+						patient.setPatientName(cell.getStringCellValue());
+						break;
+					case 1:
+						patient.setAddress(cell.getStringCellValue());
+						break;
+					case 2:
+						Date date = cell.getDateCellValue();
+						String formattedDate = dateFormatter.format(date);
+						patient.setDOB(formattedDate);
+						break;
+
+					case 3:
+						patient.setEmailId(cell.getStringCellValue());
+						break;
+					case 4:
+						System.out.println(cell.getNumericCellValue());
+						String phoneNo = NumberToTextConverter.toText(cell.getNumericCellValue());
+						patient.setPhoneNumber(phoneNo);
+						break;
+					case 5:
+						patient.setDrugId((int) cell.getNumericCellValue());
+						break;
+					case 6:
+						patient.setDrugName(cell.getStringCellValue());
+						break;
+					default:
+						break;
+					}
+					cellId++;
+
+				}
+
+				patientsList.add(patient);
+//				workbook.close();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return patientsList;
+
+	}
+
+}
